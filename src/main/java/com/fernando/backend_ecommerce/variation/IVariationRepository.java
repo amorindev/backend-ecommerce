8aab09c016775b22b6d1736659e9aeb2d14772e9
amorindev <fernandoamorindev@gmail.com>
2024-10-17 17:54:33 -0500

    CleanIVariationRepository file

--- src/main/java/com/fernando/backend_ecommerce/variation/IVariationRepository.java
+++ src/main/java/com/fernando/backend_ecommerce/variation/IVariationRepository.java
@@ -1,6 +1,5 @@
 package com.fernando.backend_ecommerce.variation;
-import java.util.List;
 
 public interface IVariationRepository {
-    List<VariationModel> findByProductId(Long productId);
+    
 }
